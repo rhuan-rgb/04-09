9dbcc0a85683dc85740cb3f0ac4a762f16b46189
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2024-09-11 16:09:35 -0300

    45min do segundo tempo O_O

--- API_rhuan860/src/controllers/controllerNumbers.js
+++ API_rhuan860/src/controllers/controllerNumbers.js
@@ -1,7 +1,7 @@
 module.exports = class controllerNumbers {
   static async checkPar(req, res) {
     const { numero } = req.body;
-    if (typeof  numero  !== "number") {
+    if (typeof numero !== "number") {
       res
         .status(400)
         .json({
@@ -18,7 +18,7 @@ module.exports = class controllerNumbers {
 
   static async checkPrimo(req, res) {
     const { numero } = req.body;
-    if (typeof { numero } !== Number) {
+    if (typeof numero !== "number") {
       res
         .status(400)
         .json({
@@ -29,16 +29,23 @@ module.exports = class controllerNumbers {
 
     let var_checkPrimo;
 
+
     //checa se o numero não é primo, retornando "true"
-    for (let i = 2; i <= Math.sqrt({ numero }); i++) {
-      if ({ numero } % 2 === 0) {
-        var_checkPrimo = true;
+    if (numero != 1){
+      for (let i = 2; i <= Math.sqrt( numero ); i++) {
+        if ( numero  % i === 0) {
+          var_checkPrimo = false;
+        }
       }
     }
 
+
     //caso o número seja primo, a variavel var_checkPrimo não terá nada dentro, logo será "false"
-    if (!var_checkPrimo) {
+    if (var_checkPrimo === false) {
       res.send({ message: `o número ${numero} é primo` });
+    }
+    else{
+      res.send({ message: `o número ${numero} não é primo` });
     }
   }
 }
